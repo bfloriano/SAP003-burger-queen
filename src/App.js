17bbc754b50d99ab6866d218f4c8eb12309b8408
bfloriano <b.floriano@outlook.com>
2019-12-11 04:32:38 -0300

    link com firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
+
+import firebase from './components/Firebase/firebase';
 import './App.css';
 
 function App() {
+
+  // PARA LER
+  // firebase.firestore().collection('user').doc('breakfast')
+  // .collection('item').doc('1')
+  // .get().then((doc => {
+  //   console.log(doc.data());
+  // })); 
+
+  firebase.firestore().collection('allDay').doc('item').collection('Hambúrgueres')
+  .get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data().name}`);
+    });
+});
+   
+
+    // PARA ADD
+    // firebase.firestore().collection("users").add({
+    //   first: "Ada",
+    //   last: "Lovelace", 
+    //   born: 1815
+    // })
+    // .then(function(docRef) {
+    //     console.log("Document written with ID: ", docRef.id);
+    // })
+    // .catch(function(error) {
+    //     console.error("Error adding document: ", error);
+    // });
+  
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,8 +51,11 @@ function App() {
           Learn React
         </a>
       </header>
+      
     </div>
+  
   );
+
 }
 
 export default App;
